ba0ddb13eecb771965226ba63d8f16c1f5a3074d
davewaldorf <dave.waldorf@me.com>
2023-10-07 22:10:39 +0200

    feat: CircleCI config file is added

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,5 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'ctmvsx',
-})
+  projectId: "ctmvsx",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
