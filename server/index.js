f37252d0faedb32520500d520cbd042abba9a7b4
davewaldorf <dave.waldorf@me.com>
2024-12-02 14:55:30 +0100

    feat: update Vercel configuration to use Docker and enhance start script

--- server/index.js
+++ server/index.js
@@ -10,19 +10,15 @@ const corsConfig = {
   credentials: true,
 };
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-// Enable cross-origin resource sharing
+
 app.use(cors(corsConfig));
-// Parse request bodies as JSON
 app.use(bodyParser.json());
 
-// Mount routes
 app.use(routes);
 
-// Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
